30ce022a2f110ebc3c1895f2d0d5e2881e6fc0ad
Sofia Breganni <sofia.breganni@gmail.com>
2026-05-09 01:13:35 +0200

    adding graphs to side panel in map and initial stats

--- app/routes/map.py
+++ app/routes/map.py
@@ -1,9 +1,11 @@
-from flask import Blueprint, render_template, send_from_directory, send_file
+from flask import Blueprint, render_template, send_from_directory
 from pathlib import Path
-import os
 
 map_bp = Blueprint('map', __name__)
+
 ROOT = Path(__file__).parent.parent.parent
+MAP_DIR = Path(ROOT / 'app/static/map')
+CHARTS_DIR = Path(ROOT / 'app/static/charts')
 
 @map_bp.route('/')
 def index():
@@ -15,17 +17,24 @@ def map():
 
 @map_bp.route('/api/country-rates')
 def country_rates():
-    return send_from_directory(Path(ROOT / 'app/static/map'), 'country_rates.json')
+    return send_from_directory(MAP_DIR, 'country_rates.json')
 
 @map_bp.route('/api/corridors')
 def corridors():
-    return send_from_directory(Path(ROOT / 'app/static/map'), 'corridors.json')
+    return send_from_directory(MAP_DIR, 'corridors.json')
 
-# adding routes for the sidebar graphs
 @map_bp.route('/api/charts/top-senders')
 def top_senders_chart():
-    return send_file('data/processed/charts/top_sender_countries_chart.json')
+    return send_from_directory(CHARTS_DIR, 'top_sender_countries_chart.json')
 
 @map_bp.route('/api/charts/top-receivers')
 def top_receivers_chart():
-    return send_file('data/processed/charts/top_receiver_countries_chart.json')
+    return send_from_directory(CHARTS_DIR, 'top_receiver_countries_chart.json')
+
+@map_bp.route('/api/charts/hourly')
+def hourly_chart():
+    return send_from_directory(CHARTS_DIR, 'hourly_chart.json')
+
+@map_bp.route('/api/charts/top-corridors')
+def top_corridors_chart():
+    return send_from_directory(CHARTS_DIR, 'top_corridors_chart.json')
