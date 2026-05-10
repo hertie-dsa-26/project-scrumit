fe245d5b119b2d87cb1be8a8a0e638341a8516e0
adjicisse1 <adji.cisse15@gmail.com>
2026-05-10 12:22:20 +0200

    sidebar

--- app/routes/map.py
+++ app/routes/map.py
@@ -35,6 +35,6 @@ def top_receivers_chart():
 def hourly_chart():
     return send_from_directory(CHARTS_DIR, 'hourly_chart.json')
 
-@map_bp.route('/api/charts/top-corridors')
-def top_corridors_chart():
-    return send_from_directory(CHARTS_DIR, 'top_corridors_chart.json')
+@map_bp.route('/api/charts/domestic_vs_crossborder')
+def domestic_vs_crossborder_chart():
+    return send_from_directory(CHARTS_DIR, 'domestic_vs_crossborder_chart.json')
